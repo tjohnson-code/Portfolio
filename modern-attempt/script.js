489a323a01fea4cc80eccec6cdd3fac3c23c5075
Tim Johnson <tjjohnson.86@gmail.com>
2023-04-22 23:22:00 -0500

    updated styles, added floating menu

--- modern-attempt/script.js
+++ modern-attempt/script.js
@@ -1,42 +1,70 @@
 'use strict';
+const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
+const navHeight = nav.getBoundingClientRect().height;
+const sections = [
+  'about',
+  'news',
+  'indiv-services',
+  'prod-services',
+  'careers',
+  'contact',
+];
 
-document.addEventListener('DOMContentLoaded', () => {
-  const sections = [
-    'about',
-    'news',
-    'indiv-services',
-    'prod-services',
-    'careers',
-    'contact',
-  ];
+const floatingMenu = function (entries) {
+  const [entry] = entries;
+  console.log(navHeight);
+  if (!entry.isIntersecting) nav.classList.add('floating');
+  else nav.classList.remove('floating');
+};
 
-  // Function to load JSON content
-  function loadContent(sectionId, contentName) {
-    fetch(`./content/${contentName}.json`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(
-            `Error loading ${contentName}: ${response.statusText}`
-          );
-        }
-        return response.json();
-      })
-      .then((content) => {
-        const sectionContent = document.querySelector(`#${sectionId}-content`);
+const headerObserver = new IntersectionObserver(floatingMenu, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
 
-        // Check if content is an array, if not convert it to one
-        const contentArray = Array.isArray(content) ? content : [content];
+headerObserver.observe(header);
 
-        const titleDiv = document.createElement('div');
-        const scrollDiv = document.createElement('div');
-        scrollDiv.classList.add('scroll-container');
+// Function to load JSON content by section and tab name
+// function loadContent(sectionId, contentName) {
+//   fetch(`./content/${contentName}.json`)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error(
+//           `Error loading ${contentName}: ${response.statusText}`
+//         );
+//       }
+//       return response.json();
+//     })
+//     .then((content) => {
+//       const sectionContent = document.querySelector(`#${sectionId}-content`);
 
-        contentArray.forEach((section) => {
-          if (section.title) {
-            titleDiv.innerHTML += `<h2>${section.title}</h2>`;
-          }
+// Function refactored using async/await
+async function loadContent(sectionId, contentName) {
+  try {
+    const response = await fetch(`./content/${contentName}.json`);
 
-          scrollDiv.innerHTML += `
+    if (!response.ok) {
+      throw new Error(`Error loading ${contentName}: ${response.statusText}`);
+    }
+
+    const content = await response.json();
+    const sectionContent = document.querySelector(`#${sectionId}-content`);
+
+    // Check if content is an array, if not convert it to one
+    const contentArray = Array.isArray(content) ? content : [content];
+
+    const titleDiv = document.createElement('div');
+    const scrollDiv = document.createElement('div');
+    scrollDiv.classList.add('scroll-container');
+
+    contentArray.forEach((section) => {
+      if (section.title) {
+        titleDiv.innerHTML += `<h2>${section.title}</h2>`;
+      }
+
+      scrollDiv.innerHTML += `
             ${section.heading ? `<h3>${section.heading}</h3>` : ''}
             ${section.content1 ? `<p>${section.content1}</p>` : ''}
             ${section.content2 ? `<p>${section.content2}</p>` : ''}
@@ -52,46 +80,49 @@ document.addEventListener('DOMContentLoaded', () => {
                 : ''
             }
           `;
-        });
-
-        sectionContent.innerHTML = '';
-        sectionContent.appendChild(titleDiv);
-        sectionContent.appendChild(scrollDiv);
-      })
-      .catch((error) => {
-        console.error('Error loading content:', error);
-      });
-  }
-
-  sections.forEach((sectionId) => {
-    const tabs = document.querySelectorAll(
-      `#${sectionId} .tabs .tab, #${sectionId} .tabs-drop-down .tab`
-    );
-
-    // Event listeners for all tabs
-    tabs.forEach((tab) => {
-      tab.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        // Remove the 'active' class from all tabs in this section
-        tabs.forEach((otherTab) => {
-          otherTab.classList.remove('active');
-        });
-
-        // Add the 'active' class to the clicked tab
-        tab.classList.add('active');
-
-        const contentName = event.target.dataset.content;
-        loadContent(sectionId, contentName);
-      });
     });
 
-    // Set the first tab as active and load its content by default
-    if (tabs.length > 0) {
-      tabs[0].classList.add('active');
-      loadContent(sectionId, tabs[0].dataset.content);
-    }
+    sectionContent.innerHTML = '';
+    sectionContent.appendChild(titleDiv);
+    sectionContent.appendChild(scrollDiv);
+  } catch (error) {
+    console.error('Error loading content:', error);
+  }
+}
+//     .catch((error) => {
+//       console.error('Error loading content:', error);
+//     });
+// }
+
+sections.forEach((sectionId) => {
+  const tabs = document.querySelectorAll(
+    `#${sectionId} .tabs .tab, #${sectionId} .tabs-drop-down .tab`
+  );
+
+  // Event listeners for all tabs
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      // Remove the 'active' class from all tabs in this section
+      tabs.forEach((otherTab) => {
+        otherTab.classList.remove('active');
+      });
+
+      // Add the 'active' class to the clicked tab
+      tab.classList.add('active');
+
+      const contentName = event.target.dataset.content;
+      loadContent(sectionId, contentName);
+    });
   });
+
+  // Set the first tab as active and load its content by default
+  if (tabs.length > 0) {
+    const [defaultTab] = tabs;
+    defaultTab.classList.add('active');
+    loadContent(sectionId, defaultTab.dataset.content);
+  }
 });
 
 // Add function for drop-down menu
