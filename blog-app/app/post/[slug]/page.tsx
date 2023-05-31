1ac7631a510ed937337d5795f996d4590d868c0f
Tim Johnson <tjjohnson.86@gmail.com>
2023-05-31 12:05:55 -0500

    updated post formatting

--- blog-app/app/post/[slug]/page.tsx
+++ blog-app/app/post/[slug]/page.tsx
@@ -1,5 +1,8 @@
 import { groq } from 'next-sanity';
 import { client } from '../../../lib/sanity.client';
+import Image from 'next/image';
+import { PortableText } from '@portabletext/react';
+import urlFor from '@/lib/urlFor';
 
 type Props = {
   params: {
@@ -18,11 +21,22 @@ export default async function Post({ params: { slug } }: Props) {
   const post: Post = await client.fetch(query, { slug });
 
   return (
-    <article>
+    <article className="p-5">
       <section>
-        <div className="text-amber-100/80">
-          <h1>{post.title}</h1>
-          <p>{post.author.name}</p>
+        <div className="text-amber-100/80 flex flex-col justify-center items-center max-w-7xl space-x-3">
+          <div className="w-72 h-auto">
+            <Image
+              className="w-full h-auto rounded-lg"
+              src={urlFor(post.mainImage).url()}
+              alt={post.title}
+              width={250}
+              height={300}
+              sizes="100vw"
+            />
+          </div>
+          <h1 className=" text-xl font-bold">{post.title}</h1>
+          <p className="italic p-2">{post.author.name}</p>
+          <PortableText value={post.body} />
         </div>
       </section>
     </article>
